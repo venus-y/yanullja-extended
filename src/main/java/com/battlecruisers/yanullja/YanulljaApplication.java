a21f69cb682ab47274d2ece37a6f16e75da60eed
Daehyun Kim <18080546+vimkim@users.noreply.github.com>
2024-01-11 10:19:47 +0900

    feat(*): initial Spring Boot setup (#32)
    * feat(*): initial Spring Boot setup
    
    * fix(build): not yet docker
    
    * feat(*.properties): add postresql properties, env, and gitignore all properties except application.properties

---
+++ src/main/java/com/battlecruisers/yanullja/YanulljaApplication.java
@@ -0,0 +1,13 @@
+package com.battlecruisers.yanullja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanulljaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YanulljaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/battlecruisers/yanullja/YanulljaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battlecruisers.yanullja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanulljaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
