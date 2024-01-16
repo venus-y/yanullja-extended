268000b909134f3c1ad1adc1b30735b9758169b3
hyunwoo0318 <97041290+hyunwoo0318@users.noreply.github.com>
2024-01-16 12:46:25 +0900

    feat(QueryDsl): initial QueryDsl setup (#54)

---
+++ src/main/java/com/battlecruisers/yanullja/config/QueryDslConfig.java
@@ -0,0 +1,22 @@
+package com.battlecruisers.yanullja.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class QueryDslConfig {
+
+    private final EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+}
