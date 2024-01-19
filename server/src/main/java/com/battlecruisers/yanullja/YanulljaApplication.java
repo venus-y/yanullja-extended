0ea29c471e7e9eeead859573c56bddb097775ae7
Daehyun Kim <18080546+vimkim@users.noreply.github.com>
2024-01-19 16:05:34 +0900

    feat(swagger): allow / as server path (#83)

--- server/src/main/java/com/battlecruisers/yanullja/YanulljaApplication.java
+++ server/src/main/java/com/battlecruisers/yanullja/YanulljaApplication.java
@@ -1,15 +1,18 @@
 package com.battlecruisers.yanullja;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server Url")})
 public class YanulljaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YanulljaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(YanulljaApplication.class, args);
+    }
 
 }
