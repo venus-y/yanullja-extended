e26bd7f4c6770075910d7ea3951764eb2f243629
Daehyun Kim <18080546+vimkim@users.noreply.github.com>
2024-02-21 15:09:55 +0900

    docs(auth): add Auth / Member Controller Swagger annotations (#153)
    * build(gradle): clean up gradle & organize
    
    * logback xml must be exempted from formatting
    
    * Add Swagger annotations to AuthController
    
    * Refactor docs of auth and member controllers with swagger annotations

--- server/src/main/java/com/battlecruisers/yanullja/auth/controller/AuthController.java
+++ server/src/main/java/com/battlecruisers/yanullja/auth/controller/AuthController.java
@@ -2,14 +2,19 @@ package com.battlecruisers.yanullja.auth.controller;
 
 import com.battlecruisers.yanullja.auth.dto.LoginDto;
 import com.battlecruisers.yanullja.member.MemberService;
-import jakarta.servlet.http.HttpServletRequest;
-import java.security.Principal;
+import com.battlecruisers.yanullja.member.domain.SecurityMember;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,21 +27,57 @@ public class AuthController {
     private final MemberService memberService;
 
     @GetMapping
-    public ResponseEntity<Principal> auth(Principal principal) {
-
-        if (principal == null) {
-            throw new RuntimeException("User not logged in!");
-        }
-        return ResponseEntity.ok(principal);
+    @Operation(summary = "Get Current User", description = "Retrieves the current authenticated user's details. User must be authenticated.")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved the current user's details.")
+    @ApiResponse(responseCode = "401", description = "User not logged in - Unauthorized access.")
+    public ResponseEntity<SecurityMember> getCurrentUserDetails(
+        @AuthenticationPrincipal SecurityMember member) {
+        return ResponseEntity.ok(member);
     }
 
     @PostMapping("/login")
-    public void login(@RequestBody LoginDto loginDto,
-        HttpServletRequest request) {
+    @Operation(summary = "Login",
+        description = "Performs user login.",
+        requestBody = @RequestBody(
+            description = "Login credentials",
+            required = true,
+            content = @Content(
+                examples = {
+                    @ExampleObject(
+                        name = "login",
+                        value = """
+                            {
+                                "email": "mockuser@mock.com",
+                                "password": "mockuser"
+                            }
+                            """
+                    ),
+                    @ExampleObject(
+                        name = "login fail",
+                        value = """
+                            {
+                                "email": "you cannot",
+                                "password": "login"
+                            }
+                            """
+                    )
+
+                },
+                schema = @Schema(implementation = LoginDto.class)
+            )
+        ),
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Successfully authenticated"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized - Invalid credentials")
+        }
+    )
+    public void login(@RequestBody LoginDto loginDto) {
         throw new RuntimeException("Login is handled by Spring Security");
     }
 
     @PostMapping("/logout")
+    @Operation(summary = "Logout", description = "Performs user logout.")
+    @ApiResponse(responseCode = "200", description = "Successfully logged out")
     public void logout() {
         throw new RuntimeException("Logout is handled by Spring Security");
     }
--- server/src/main/java/com/battlecruisers/yanullja/member/MemberController.java
+++ server/src/main/java/com/battlecruisers/yanullja/member/MemberController.java
@@ -3,6 +3,10 @@ package com.battlecruisers.yanullja.member;
 import com.battlecruisers.yanullja.member.domain.SecurityMember;
 import com.battlecruisers.yanullja.member.dto.MemberResponseDto;
 import com.battlecruisers.yanullja.member.dto.MemberUpdateDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +23,14 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/me")
+    @Operation(summary = "Get my information",
+        description = "Returns member information for the currently authenticated user",
+        responses = {
+            @ApiResponse(description = "Successful retrieval of user information",
+                responseCode = "200",
+                content = @Content(schema = @Schema(implementation = MemberResponseDto.class))),
+            @ApiResponse(description = "Unauthorized if the user is not authenticated", responseCode = "401")
+        })
     public MemberResponseDto getMyInfo(
         @AuthenticationPrincipal SecurityMember me) {
 
@@ -27,6 +39,15 @@ public class MemberController {
     }
 
     @PutMapping("/me")
+    @Operation(summary = "Update my information",
+        description = "Updates the member information for the currently authenticated user",
+        responses = {
+            @ApiResponse(description = "Successful update of user information", responseCode = "200"),
+            @ApiResponse(description = "Unauthorized if the user is not authenticated", responseCode = "401")
+        },
+        requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Member update information",
+            required = true,
+            content = @Content(schema = @Schema(implementation = MemberUpdateDto.class))))
     public void updateMyInfo(@AuthenticationPrincipal SecurityMember member,
         @RequestBody MemberUpdateDto dto) {
         var memberId = member.getId();
